a2c5a922c764c3c160431ba1271f8c3c511bc615
Bernard Calma <nard.calma@gmail.com>
2023-04-14 10:08:22 -0700

    api call from client to server tested

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
+import axios from 'axios'
 import { useState } from 'react'
 import './App.css';
 
 const App = () => {
   const [testVar, setTestVar] = useState("Test")
   const apiCall = () => {
-    fetch("http://localhost:8000")
-    .then(res => console.log(res))
+    axios({
+      method: "GET",
+      url: "http://localhost:8000"
+    })
+    .then(res => setTestVar(res.data))
     .catch(err => console.log(err))
   }
   return <div>
--- server/server.js
+++ server/server.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const cors = require('cors')
+const session = require('express-session')
 
 // ----- MIDDLEWARES -----
 app.use(express.static(path.join(__dirname + "public")))
+app.use(cors({
+    credentials: true,
+    option: ['http://localhost:3000']
+}))
 // ----- END OF MIDDLEWARES -----
 const PORT = process.env.PORT || 8000
 
